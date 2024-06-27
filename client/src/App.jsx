5beb1652f133d526f95954057ab75500b2d8f64e
Mariklimova <mariku22@yandex.ru>
2024-06-28 00:23:57 +0300

    work with home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-
-import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import './App.css';
+import Home from './pages/HomePage/Home';
 
 function App() {
-  
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path = '/' element = {<Home/>}></Route>
+      </Routes>
+    
+    </div>
   )
 }
 
---
+++ client/src/pages/HomePage/Home.jsx
@@ -0,0 +1,18 @@
+import style from './home.module.css'
+
+function Home() {
+    return <>
+        <div className={style.blok_left}>
+            <h1>HOUSE OF</h1>
+            <h2>GAMBLING</h2>
+            <p>Raise your ROI with direct advertiser</p>
+            <button>Become a Partner</button>
+
+        </div>
+
+        <div className={style.blok_center}>
+
+        </div>
+    </>
+}
+export default Home
