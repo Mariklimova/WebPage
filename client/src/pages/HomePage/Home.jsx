35168824897c2151a317c03b8afe646fd91e03d8
Mariklimova <mariku22@yandex.ru>
2024-07-01 02:55:52 +0300

    work with home page & animation

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -6,11 +6,51 @@ function Home() {
             <h1>HOUSE OF</h1>
             <h2>GAMBLING</h2>
             <p>Raise your ROI with direct advertiser</p>
-            <button>Become a Partner</button>
+            <button>BECOME A PARTNER</button>
 
         </div>
 
         <div className={style.blok_center}>
+            <div className={style.line_left}></div>
+            <div className={style.line_center}></div>
+            <div className={style.line_right}></div>
+        </div>
+
+        <div className={style.blok_right}>
+            <h3>BLOG</h3>
+            <div className={style.container}>
+                <div className={style.scroll_bar}>
+                    <div className={style.text}>
+                        <div className={style.text_blok}>
+                            <h5>Payment methods: Skrill, Neteller, webmoney, Bank transfer</h5>
+                            <p>10.02.2023</p>
+                        </div>
+
+                        <div className={style.text_blok}>
+                            <h5>Payment methods: Skrill, Neteller, webmoney, Bank transfer</h5>
+                            <p>10.02.2023</p>
+                        </div>
+
+                        <div className={style.text_blok}>
+                            <h5>Payment methods: Skrill, Neteller, webmoney, Bank transfer</h5>
+                            <p>10.02.2023</p>
+                        </div>
+
+                        <div className={style.text_blok}>
+                            <h5>Payment methods: Skrill, Neteller, webmoney, Bank transfer</h5>
+                            <p>10.02.2023</p>
+                        </div>
+
+                        <div className={style.text_blok}>
+                            <h5>Payment methods: Skrill, Neteller, webmoney, Bank transfer</h5>
+                            <p>10.02.2023</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
     </>
