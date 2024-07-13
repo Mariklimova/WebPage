c1bcc9a31eb1b731337f601ae40d57966d668e48
Mariklimova <mariku22@yandex.ru>
2024-07-13 21:08:51 +0300

    added footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,48 @@
 import style from './footer.module.css'
 
 function Footer() {
-    
+    return <>
+        <div className={style.footer}>
+            <div className={style.navFooter}>
+                <p>Terms & Conditions</p>
+                <p>Cookies</p>
+                <p>Contacts</p>
+                <p>Careers</p>
+                <p>Brand Guide</p>
+            </div>
+            <div className={style.info}>
+                <p>Our social media:</p>
+
+                <div className={style.wrapperIcons}>
+                    <div className={style.fonIcon}>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.twitter}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.facebook}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.instagram}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.linked}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.youtube}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.tiktok}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.telegram}></div>
+                        </div>
+                        <div className={style.fonIconBlue} >
+                            <div className={style.vk}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    </>
 }
 export default Footer;
