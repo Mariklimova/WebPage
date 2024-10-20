e97b3bd832fcefff980f55f1c312e4bedc8c3393
Mariklimova <mariku22@yandex.ru>
2024-10-21 02:11:57 +0300

    work with adaptive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                 {['About us', 'Brands', 'Commissions', 'News', 'Contact us', 'Careers'].map((el, i) => <p key={i}>{el}</p>)}
             </div>
             <div className={style.buttons}>
-                <button className={style.language_button} onClick={changeLanguage}><div className={style.icon}></div>{language}</button>
+                <button className={style.language_button} onClick={changeLanguage}><div className={style.icon}></div><p className={style.language_name}>{language}</p></button>
                 <button className={style.drop_down_menu} onClick={toggleMenu}><div className={style.icon_burger}></div></button>
                 <button className={style.btn_blue}>LOG IN</button>
                 <button className={style.btn_blue}>SIGN UP</button>
